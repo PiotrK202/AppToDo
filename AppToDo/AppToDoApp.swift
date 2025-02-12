69a46f8fc5144602d2f710ec545d2b8b7fd7de49
PiotrK202 <piotr.koscielny69@gmail.com>
2025-02-12 15:46:59 +0100

    Initial Commit

---
+++ AppToDo/AppToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppToDoApp.swift
+//  AppToDo
+//
+//  Created by piotr koscielny on 12/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppToDo
+//
+//  Created by piotr koscielny on 12/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
