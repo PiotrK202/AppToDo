7ae7aee92a60024d5eb7113bc09cf5911d15d2ae
PiotrK202 <piotr.koscielny69@gmail.com>
2025-02-12 16:15:06 +0100

    Updated ListRowView

--- AppToDo/Views/ListRawView.swift
+++ AppToDo/Views/ListRawView.swift
@@ -13,9 +13,13 @@ struct ListRowView: View {
     
     var body: some View {
         HStack {
-            Image("checkmark.circle")
+            Image(systemName: item.isCompleted ? "checkmark.circle" : "circle")
+                .foregroundColor(item.isCompleted ? .green : .red)
             Text(item.title)
+            Spacer()
         }
+        .font(.title2)
+        .padding(.vertical)
     }
 }
 
