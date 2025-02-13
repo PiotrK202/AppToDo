cd34c1c879140d956b6146590687056efdb0460f
PiotrK202 <piotr.koscielny69@gmail.com>
2025-02-13 10:12:01 +0100

    Added func to check if text that is added is good

--- AppToDo/Views/AddView.swift
+++ AppToDo/Views/AddView.swift
@@ -13,6 +13,9 @@ struct AddView: View {
     @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText = ""
     
+    @State var alertTitle = ""
+    @State var showAlert: Bool = false
+    
     var body: some View {
         ScrollView {
             TextField("type what to add", text: $textFieldText)
@@ -34,11 +37,27 @@ struct AddView: View {
             
         }
         .navigationTitle("add an item")
+        .alert(isPresented: $showAlert, content: getAlert )
     }
     
     func saveButtonPressed() {
+        if textIsAppropriate() {
         listViewModel.addItem(title: textFieldText)
         presentationMode.wrappedValue.dismiss()
+    }
+    }
+    
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "your to do item must be at least 3 characters long."
+            showAlert.toggle()
+          return  false
+        }
+        return true
+    }
+    
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
     }
     
 }
